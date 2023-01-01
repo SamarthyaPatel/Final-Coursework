c9bed178660cd9e1dcb6e710cff3e4b3e2766ea8
Samarthya Patel <samarthyapatel04@gmail.com>
2023-01-01 21:18:26 +0000

    Posts are displayed using VC

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,7 +14,7 @@ class PostController extends Controller
      */
     public function index()
     {
-        $posts = Post::all();
+        $posts = Post::get();
         return view('posts.index', ['posts' => $posts]);
     }
 
--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -5,7 +5,9 @@
 @section('content')
     <P>The posts on the social platform</P>
     <ul>
-        <li>Post 1</li>
-        <li>Post 2</li>
+        @foreach($posts as $post)
+            <li>{{ $post->caption }}</li>
+        @endforeach
     </ul>
+
 @endsection
